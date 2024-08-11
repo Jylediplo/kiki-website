02c1ae6a17e8d37f9dd455d241f0896f5aec76a1
luko <lucasfabregoule@gmail.com>
2024-08-11 16:43:15 +0200

    horaires

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function HomePage() {
   return (
     <header className="flex flex-col items-center justify-between p-8">
       <div className="z-10 w-full max-w-7xl flex flex-col md:flex-row items-start justify-between font-bold text-sm space-y-4 md:space-y-0 md:space-x-8">
-        <div className="w-full md:w-1/2">
+        <div className="w-full md:w-1/4">
           <StoreHours />
         </div>
-        <div className="w-full md:w-1/2">
+        <div className="w-full md:w-3/4">
           <Map />
         </div>
       </div>
