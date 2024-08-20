198dd6801e1db37c553f296fc63447ad871e367e
JyJy <leofabregoule@gmail.com>
2024-08-20 15:50:02 +0200

    change next build comment in package.json

--- app/api/db/route.ts
+++ app/api/db/route.ts
@@ -1,22 +1,32 @@
-// pages/api/createItem.js
-import { db } from './firebase';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+// // pages/api/createItem.js
+// import { db } from './firebase';
+// import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+// import { NextResponse } from 'next/server';
 
-export default async (req:any, res:any) => {
-  if (req.method === 'POST') {
-    const { item } = req.body;
+// export const GET = async (req:any, res:any) => {
+//     try {
+//         const rawResponse = await fetch('http://localhost:3000/api/products')
+//         const response = await rawResponse.json()
+//         console.log(response);
+//         return new NextResponse(JSON.stringify({ id: docRef.id }), { status: 200 });
+        
+//     }
+//     catch (error) {
+//         return new NextResponse("Error fetching data: " + error);
+//     }
+//     try {
+//         // const body = await req.json();  // Parse the request body
+//         // // console.log(body);
 
-    try {
-      const docRef = await addDoc(collection(db, 'items'), {
-        ...item,
-        createdAt: serverTimestamp()
-      });
+//         // // Add the document to the 'items' collection
+//         // const docRef = await addDoc(collection(db, 'items'), {
+//         //     ...body,
+//         //     createdAt: serverTimestamp()
+//         // });
 
-      res.status(201).json({ id: docRef.id });
-    } catch (error) {
-      res.status(500).json({ error: 'Error adding document: ' + error });
-    }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-};
+//         // return new NextResponse(JSON.stringify({ id: docRef.id }), { status: 200 });
+//     } catch (error) {
+//         console.error('Error adding document: ', error);
+//         return new NextResponse(JSON.stringify({error}), { status: 500 });
+//     }
+// };
